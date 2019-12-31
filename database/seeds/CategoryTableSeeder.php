cbab821bce8e142b031d974a7949b02431c681f3
tuand4 <tuannguyen321231@gmail.com>
2019-12-31 11:13:25 +0700

    fix seeder issue

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -13,25 +13,21 @@ class CategoryTableSeeder extends Seeder
     {
         DB::table('categories')->insert([
             [
-                'id' => 1,
                 'name' => 'samsung',
                 'created_at' => new DateTime,
                 'updated_at' => new DateTime,
             ],
             [
-                'id' => 2,
                 'name' => 'oppo',
                 'created_at' => new DateTime,
                 'updated_at' => new DateTime,
             ],
             [
-                'id' => 3,
                 'name' => 'apple',
                 'created_at' => new DateTime,
                 'updated_at' => new DateTime,
             ],
             [
-                'id' => 4,
                 'name' => 'nokia',
                 'created_at' => new DateTime,
                 'updated_at' => new DateTime,
